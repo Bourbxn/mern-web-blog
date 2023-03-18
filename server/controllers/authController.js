68455d03d868eb2c64b634c346e3d864a91a1b03
Bourbxn <jakkapat97@gmail.com>
2023-03-18 15:10:21 +0700

    add auth

---
+++ server/controllers/authController.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+exports.login = (req, res) => {
+  //data of username, password
+  const { username, password } = req.body;
+  if (password === process.env.PASSWORD) {
+    //Login
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    return res.json({
+      token,
+      username,
+    });
+  } else {
+    return res.status(400).json({
+      error: "Invalid Password!",
+    });
+  }
+};
